22c0951dd1e3f587b8a55eb68d4a622f53dce3dc
kvellur <karthik_vellur@intuit.com>
2020-11-03 07:50:51 +0530

    AMS - feed food to the fish

--- src/AMS.kt
+++ src/AMS.kt
@@ -28,11 +28,25 @@ fun dayOfWeek(){
 
 fun feedTheFish(){
     val day = randomDay()
-    val food = "pellets"
+    val food = fishFood(day)
     println("Today is $day and the fish eat $food")
 }
 
 fun randomDay() : String {
     val week = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
     return week[Random().nextInt(7)]
+}
+
+fun fishFood(day: String) : String {
+    var food = "fasting"
+
+    return when (day) {
+        "Monday" -> "flakes"
+        "Wednesday" -> "redworms"
+        "Thursday" -> "granules"
+        "Friday" -> "mosquitoes"
+        "Sunday" -> "plankton"
+        else -> "fasting"
+    }
+
 }
