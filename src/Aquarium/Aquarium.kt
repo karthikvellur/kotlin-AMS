22a0c9d47bdd2fccfa45bc6a8607b80ede204164
Karthik Vellur <karthikvn23@gmail.com>
2020-12-12 11:15:27 +0530

    Inheritance

--- src/Aquarium/Aquarium.kt
+++ src/Aquarium/Aquarium.kt
@@ -1,21 +1,33 @@
 package Aquarium
 
-class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40) {
+import kotlin.math.PI
+
+open class Aquarium (var length: Int = 100, var width: Int = 20, var height: Int = 40) {
 
     // Everything is public by default
     // Visibility: https://kotlinlang.org/docs/reference/visibility-modifiers.html
 
     //Over-riding default getter
-    var volume: Int
+    open var volume: Int
             get() = width * height * length / 1000
             set(value) {height = (value * 1000) / (width * length)}
 
-    var water = volume * 0.9
+    open var water = volume * 0.9
 
     constructor(numberOfFish: Int): this(){
         // this() --> Call to default constructor
-//        val water = numberOfFish * 2000 //cm3
-//        val tank = water + water * 0.1
-//        height = (tank / (length * width)).toInt()
+        val water = numberOfFish * 2000 //cm3
+        val tank = water + water * 0.1
+        height = (tank / (length * width)).toInt()
     }
+}
+
+class TowerTank(): Aquarium() {
+    override var water = volume * 0.8
+
+    override var volume: Int
+            get() = (width * height * length / 1000 * PI).toInt()
+            set(value) {
+                height = (value + 1000) / (width * length)
+            }
 }
